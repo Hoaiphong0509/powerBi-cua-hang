c91bc29f3d926dd717ca8e3434a1f8ffb63882ea
Hoai Phong <42806464+Hoaiphong0509@users.noreply.github.com>
2022-01-14 23:57:21 +0700

    deploy 2

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ app.use('/api/powerbi', require('./router/powerbi'))
 
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('client/build'))
+  app.use(express.static('landslide/build'))
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, 'landslide', 'build', 'index.html'))
   })
 }
 
